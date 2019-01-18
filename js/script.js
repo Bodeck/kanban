f4d6f15fbf58a2ac111352401142ae1777e8ad87
Bogus Franczak <bfranczak@gmail.com>
2019-01-18 17:59:53 +0100

    Create Card class and methods

--- js/script.js
+++ js/script.js
@@ -40,5 +40,24 @@ document.addEventListener('DOMContentLoaded', () => {
             this.element.parentNode.removeChild(this.element);
         }
     }
+
+    function Card(description) {
+        var self = this;
+        this.description = description;
+        this.id = randomString();
+        this.element = generateTemplate('card-template', { description: this.description }, 'li');
+        this.element.querySelector('.card').addEventListener('click', function(event){
+            event.stopPropagation();
+            if (event.targe.classList.contains('btn-delete')) {
+                self.removeCard();
+            }
+        })
+    }
+
+    Card.prototype = {
+        removeCard: function () {
+            this.element.parentNode.removeChild(this.element);
+        }
+    }
 })
 
